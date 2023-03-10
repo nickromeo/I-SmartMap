bfea5f3a6dc6be80708f696f922a70c0ae90fc8c
Krdera89 <kevin.deras@ymail.com>
2023-03-10 15:34:20 -0500

    added register and sign in privlages.

---
+++ Map/login.js
@@ -0,0 +1,41 @@
+//Method for registering user
+const signUp = e => {
+    let fname = document.getElementById('fname').value,
+        lname = document.getElementById('lname').value,
+        email = document.getElementById('email').value,
+        pwd = document.getElementById('pwd').value;
+
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData')).some(data => 
+            data.fname.toLowerCase() == fname.toLowerCase() && 
+            data.lname.toLowerCase() == lname.toLowerCase()
+        );
+
+    if(!exist){
+        formData.push({ fname, lname, email, pwd });
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('fname').focus();
+        alert("Account Created.\n\nPlease Sign in using the link below.");
+    }
+    else{
+        alert("Duplicate found!!!\nYou have already registered");
+    }
+    e.preventDefault();
+}
+//Method for signing user in
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        location.href = "/Map/map.html";
+    }
+    e.preventDefault();
+}
